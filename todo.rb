414b49514a98c7ce97219d72823c160ebe67ce44
riazmohamed <railsdevriaz@gmail.com>
2022-04-09 11:15:41 +0100

    add redirect to homepage and fix styling

--- todo.rb
+++ todo.rb
@@ -3,10 +3,13 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
-  @lists = [
-    {name: "Lunch Groceries"},
-    {name: "Dinner Groceries"}
-  ]
+  redirect "/lists"
+end
 
+get "/lists" do
+  @lists = [
+    {name: "Lunch Groceries", todos: []},
+    {name: "Dinner Groceries", todos: []}
+  ]
   erb :lists, layout: :layout
 end
