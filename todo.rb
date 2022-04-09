eeac514a6ec5ce90acb4b95914cf2c20a258d0f1
riazmohamed <railsdevriaz@gmail.com>
2022-04-09 17:17:58 +0100

    refractor the code

--- todo.rb
+++ todo.rb
@@ -30,8 +30,8 @@ end
 # create a new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if list_name.size >= 1 && list_name.size <= 100
-    session[:lists] << { name: params[:list_name], todos: [] }
+  if (1..100).cover? list_name.size
+    session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created."
     redirect "/lists"
   else
