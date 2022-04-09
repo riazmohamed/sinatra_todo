22cb0a0aa2b61e4f305bce1ddc7ad5d96131126c
riazmohamed <railsdevriaz@gmail.com>
2022-04-09 17:43:46 +0100

    Add validations for unique list

--- todo.rb
+++ todo.rb
@@ -30,12 +30,15 @@ end
 # create a new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if (1..100).cover? list_name.size
+  if !(1..100).cover? list_name.size
+    session[:error] = "List name must be between 1 and 100 characters."
+    erb :new_list, layout: :layout
+  elsif session[:lists].any? { |list| list[:name] == list_name }
+    session[:error] = "List name must be unique."
+    erb :new_list, layout: :layout
+  else
     session[:lists] << { name: list_name, todos: [] }
     session[:success] = "The list has been created."
     redirect "/lists"
-  else
-    session[:error] = "List name mus be between 1 and 100 characters."
-    erb :new_list, layout: :layout
   end
 end
