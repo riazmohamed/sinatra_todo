69171b5a52bb722019fd4bab70760dd90987fab2
riazmohamed <railsdevriaz@gmail.com>
2022-04-11 11:58:44 +0100

    add edit functionality for induvidual lists

--- todo.rb
+++ todo.rb
@@ -56,3 +56,27 @@ get "/lists/:id" do
   @list = session[:lists][id]
   erb :list, layout: :layout
 end
+
+# Edit an existing todo-list
+get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# Update an exixting todo list
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = "The list has been updated."
+    redirect "/lists/#{id}"
+  end
+end
