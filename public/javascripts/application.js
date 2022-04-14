80dbc7f601ae28fcc1f458b08a318f1a284064ac
riazmohamed <railsdevriaz@gmail.com>
2022-04-14 13:46:02 +0100

    add script to confirm destructive actions

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,15 @@
-console.log("This is a test.");
+// application.js
+
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? This cannot be undone!");
+    if (ok) {
+      this.submit();
+    }
+  });
+
+});
